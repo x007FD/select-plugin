5227923b98d14061656c81670f938404ff16a714
x007FD <vvvvvvvvvvvry@gmail.com>
2023-05-11 22:12:41 +0500

    refactor: add variables scss, update plugin class

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
-export class Select {
-    static ACTIVE_CLASS = 'is-active'
-    static SELECT_CLASS = 'ms-select'
-    static EMPTY_CLASS = 'is-empty'
-    static CHECKED_CLASS = 'is-checked'
-    static DEFAULT_LANG = 'en'
+export class VSelect {
+    ACTIVE_CLASS = 'is-active'
+    EMPTY_CLASS = 'is-empty'
+    INLINE_CLASS = 'is-inline'
+    CHECKED_CLASS = 'is-checked'
+    DEFAULT_LANG = 'en'
 
     #selectedItems
     #selectedId
@@ -24,7 +24,7 @@ export class Select {
     }
 
     get currentLang() {
-        return !this.options.lang ? Select.DEFAULT_LANG : this.options.lang.toLowerCase()
+        return !this.options.lang ? this.DEFAULT_LANG : this.options.lang.toLowerCase()
     }
 
     get currentItem() {
@@ -34,18 +34,18 @@ export class Select {
     }
 
     get isOpen() {
-        return this.selectElement.classList.contains(Select.ACTIVE_CLASS)
+        return this.selectElement.classList.contains(this.ACTIVE_CLASS)
     }
 
     clickHandler(event) {
         const target = event.target
         const targetSelectId = target.dataset.selectId
 
-        if (target.closest('.ms-select__header')) {
+        if (target.closest('.v-select__header')) {
             this.toggle()
         }
 
-        if (target.closest('.ms-select__item')) {
+        if (target.closest('.v-select__item')) {
             this.select(targetSelectId)
         }
     }
@@ -58,11 +58,11 @@ export class Select {
         headerValueElement.innerText = this.currentItem.value
         inputValueElement.value = this.#selectedId
         itemElements.forEach(item => {
-            item.classList.remove(Select.CHECKED_CLASS)
+            item.classList.remove(this.CHECKED_CLASS)
             item.disabled = false
         })
 
-        this.selectElement.querySelector(`.ms-select__item[data-select-id="${ this.#selectedId }"]`).classList.add(Select.CHECKED_CLASS)
+        this.selectElement.querySelector(`.v-select__item[data-select-id="${ this.#selectedId }"]`).classList.add(this.CHECKED_CLASS)
         this.#selectedItems.push(this.currentItem)
 
         this.close()
@@ -72,13 +72,13 @@ export class Select {
 
     open() {
         this.#executeEvent('onBeforeOpen')
-        this.selectElement.classList.add(Select.ACTIVE_CLASS)
+        this.selectElement.classList.add(this.ACTIVE_CLASS)
         this.#executeEvent('onAfterOpen')
     }
 
     close() {
         this.#executeEvent('onBeforeClose')
-        this.selectElement.classList.remove(Select.ACTIVE_CLASS)
+        this.selectElement.classList.remove(this.ACTIVE_CLASS)
         this.#executeEvent('onAfterClose')
     }
 
@@ -95,19 +95,17 @@ export class Select {
     #renderSelect() {
         const placeholder = this.#getPlaceholder()
         const items = this.data ? this.#getItems() : ''
-        const id = this.#selectedId || ''
 
         return `
-          <div class="ms-select__header">
-            <span class="ms-select__header-value">
+          <div class="v-select__header">
+            <span class="v-select__header-value">
               ${ placeholder }
             </span>
-            <svg class="ms-select__header-icon" width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg class="v-select__header-icon" width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M6.00005 6.97499C5.86672 6.97499 5.73772 6.94999 5.61305 6.89999C5.48772 6.84999 5.38338 6.78332 5.30005 6.69999L0.700048 2.09999C0.516715 1.91665 0.425049 1.68332 0.425049 1.39999C0.425049 1.11665 0.516715 0.883321 0.700048 0.699987C0.883382 0.516654 1.11672 0.424988 1.40005 0.424988C1.68338 0.424988 1.91672 0.516654 2.10005 0.699987L6.00005 4.59999L9.90005 0.699987C10.0834 0.516654 10.3167 0.424988 10.6 0.424988C10.8834 0.424988 11.1167 0.516654 11.3 0.699987C11.4834 0.883321 11.575 1.11665 11.575 1.39999C11.575 1.68332 11.4834 1.91665 11.3 2.09999L6.70005 6.69999C6.60005 6.79999 6.49172 6.87065 6.37505 6.91199C6.25838 6.95399 6.13338 6.97499 6.00005 6.97499Z" fill="#232323"/>
             </svg>
-            <input name="ms-select-option" type="hidden" class="ms-select__input" value="${ id }">
           </div>
-          <div class="ms-select__dropdown">
+          <div class="selectel__dropdown">
             ${ items }
           </div>
         `
@@ -119,11 +117,11 @@ export class Select {
         this.#executeEvent('onBeforeRender')
 
         if (!this.data || !this.data.length) {
-            this.selectElement.classList.add(Select.EMPTY_CLASS)
+            this.selectElement.classList.add(this.EMPTY_CLASS)
             console.error(translations.noItems)
         }
 
-        this.selectElement.classList.add(Select.SELECT_CLASS)
+        this.selectElement.className = `${ this.selectElement.className } v-select ${ this.options.isInline ? this.INLINE_CLASS : '' }`
         this.selectElement.innerHTML = this.#renderSelect()
 
         if (this.options.isOpen) {
@@ -151,9 +149,9 @@ export class Select {
 
     #getSelectElements() {
         return {
-            headerValueElement: this.selectElement.querySelector('.ms-select__header-value'),
-            inputValueElement: this.selectElement.querySelector('.ms-select__input'),
-            itemElements: this.selectElement.querySelectorAll('.ms-select__item')
+            headerValueElement: this.selectElement.querySelector('.v-select__header-value'),
+            inputValueElement: this.selectElement.querySelector('.v-select__input'),
+            itemElements: this.selectElement.querySelectorAll('.v-select__item')
         }
     }
 
@@ -187,13 +185,13 @@ export class Select {
         const sortedItems = sortFunctions[this.options.sortType || 'default'](this.data)
 
         return sortedItems.map((item, index) => `
-              <button class="ms-select__item ${ this.#selectedId === +item.id ? Select.CHECKED_CLASS : '' }"
+              <button class="v-select__item ${ this.#selectedId === +item.id ? this.CHECKED_CLASS : '' }"
                       data-select-id="${ item.id }"
                       ${ this.#selectedId === +item.id ? 'disabled' : '' }>
-                      <span class="ms-select__item-value">
+                      <span class="v-select__item-value">
                         ${ this.options.isOrder ? `${ index + 1 }.` : '' } ${ item.value }           
                       </span>
-                      <span class="ms-select__item-info">
+                      <span class="v-select__item-info">
                         ${ item.info ?? '' }      
                       </span>
               </button>
