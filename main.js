9c551faa19a52cab974e1a6b72a954cf1949ff1d
x007FD <vvvvvvvvvvvry@gmail.com>
2023-05-11 23:01:36 +0500

    chore: update main script

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-import './src/style.scss'
+import './src/style.scss';
 import { VSelect } from "./src/script.js";
 
 const select = new VSelect('#my-select', {
     data: [
-        { id: 1, value: 'Vue', info: 30 },
+        { id: 1, value: 'Vue' },
         { id: 2, value: 'React' },
     ],
+    isInline: true,
     isOpen: true,
     sortType: 'desc',
     onSelect(prev, next) {
